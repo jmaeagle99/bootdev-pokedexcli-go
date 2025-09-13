e3e4b910130b133bf82c29b329b34b32ae0076dd
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2025-09-12 17:09:49 -0700

    Add cleanInput

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	parts := strings.Split(text, " ")
+	var result []string
+	for _, part := range parts {
+		if part != "" {
+			result = append(result, part)
+		}
+	}
+	return result
 }
---
+++ repl_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+    tests := []struct {
+		name     string
+		input    string
+		expected []string
+	}{
+		{
+			name:     "multiple-spaces",
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			name:     "multiple-words",
+			input:    "Bulbasaur Charmander Squirtle PIKACHU",
+			expected: []string{"Bulbasaur", "Charmander", "Squirtle", "PIKACHU"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := cleanInput(tt.input)
+			if len(actual) != len(tt.expected) {
+				t.Errorf("Expected length %d, Actual length %d", len(tt.expected), len(actual))
+			} else {
+				for i := range actual {
+					actualWord := actual[i]
+					expectedWord := tt.expected[i]
+					
+					if expectedWord != actualWord {
+						t.Errorf("At index %d, expected '%s', actual '%s'", i, expectedWord, actualWord)
+					}
+				}
+			}
+		})
+	}
+}
